0e0de7d1934bdfd4918f29693c9cae16074960a1
YoussefAchraf <135220512+YoussefAchraf@users.noreply.github.com>
2024-05-01 20:23:56 +0200

    new commit

--- src/pages/components/FoodCard.js
+++ src/pages/components/FoodCard.js
@@ -1,4 +1,16 @@
+import React, { useState } from "react";
+
 const FoodCard = (props) => {
+  const [showShareOptions, setShowShareOptions] = useState(false);
+
+  const handleShareClick = () => {
+    setShowShareOptions(true);
+  };
+
+  const handleCancelClick = () => {
+    setShowShareOptions(false);
+  };
+
   return (
     <div className="max-w-xs mx-auto rounded overflow-hidden shadow-lg">
       <img className="w-full h-48 object-cover" src={props.img} alt={props.imgAlt} />
@@ -8,12 +20,32 @@ const FoodCard = (props) => {
         <p className="text-[#ee9972] text-xl mt-[1dvh] font-medium">{props.foodPrice}</p>
       </div>
       <div className="px-6 py-4">
-        <button className="bg-[#495e57] text-white font-bold py-2 px-4 rounded mr-2">
-          Share
-        </button>
-        <button className="bg-[#F4CE15] text-white font-bold py-2 px-4 rounded">
-          Order now
-        </button>
+        {showShareOptions ? (
+          <div className="text-center">
+            <p className="mb-2 font-bold text-gray-700">Share on :</p>
+            <div>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-1 rounded mx-1">
+                Facebook
+              </button>
+              <button className="bg-pink-500 hover:bg-pink-700 text-white font-semibold py-1 px-1 rounded mx-1">
+                Instagram
+              </button>
+              <button className="bg-gray-500 hover:bg-gray-700 text-white font-semibold py-1 px-1 rounded mx-1">
+                X(twitter)
+              </button>
+            </div>
+            <p className="mt-2 cursor-pointer text-[#F4CE15] underline" onClick={handleCancelClick}>Cancel</p>
+          </div>
+        ) : (
+          <div>
+            <button onClick={handleShareClick} className="bg-[#495e57] text-white font-bold py-2 px-4 rounded mr-2">
+              Share
+            </button>
+            <button className="bg-[#F4CE15] text-white font-bold py-2 px-4 rounded">
+              Order now
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
