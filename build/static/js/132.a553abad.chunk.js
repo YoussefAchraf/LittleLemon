7ae9169a14a882313fc70c42fdf347e9cb6fb744
YoussefAchraf <135220512+YoussefAchraf@users.noreply.github.com>
2024-06-11 02:01:00 +0200

    new commit

---
+++ build/static/js/132.a553abad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklittlelemon=self.webpackChunklittlelemon||[]).push([[132],{1132:(e,t,l)=>{l.r(t),l.d(t,{default:()=>c});l(5043);var n=l(3216),s=l(579);const c=e=>{const t=(0,n.Zp)();return(0,s.jsx)("button",{className:"w-[160px] h-[50px] rounded-[50px] bg-[#f4ce14] text-[white] font-medium",onClick:()=>{t(e.page)},children:e.txt})}}}]);
+//# sourceMappingURL=132.a553abad.chunk.js.map
